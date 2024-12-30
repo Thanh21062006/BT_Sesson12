5a992b919e3acfac8961ea2dac945a6e2010f290
Thanh21062006 <caotatthanh2006@gmail.com>
2024-12-30 10:19:18 +0700

    Update BT4.cpp

--- BT4.cpp
+++ BT4.cpp
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
-int array[10] = {1, 3, 6, 22, 9, 7, 8, 0, 10, 20};
-int maxArray(void);
+int maxArray(int array[], int size);
 
 int main() {
-	printf("So lon nhat trong mang la: %d",maxArray());
+	int array[10] = {1, 3, 6, 22, 9, 7, 8, 0, 10, 20};
+	int size = sizeof(array)/sizeof(int);
+	printf("So lon nhat trong mang la: %d",maxArray(array, size));
 	return 0;
 }
 
-int maxArray(void){
+int maxArray(int array[], int size){
 	int max = 0;
-	for(int i = 0;i < sizeof(array)/sizeof(int);i++){
+	for(int i = 0;i < size;i++){
 		if(array[i] > max){
 			max = array[i];
 		}
 	}
 	return max;
-}
+};
